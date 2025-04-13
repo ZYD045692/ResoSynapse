a7f1e25d2299d422572c021f3c3006d4a86ec3a2
ZYD045692 <2214845593@qq.com>
2025-04-13 21:11:07 +0800

    重新上传文件为普通Git文件

--- setup/lead.bat
+++ setup/lead.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d "../frontend/lead"
 call npm cache clean --force
-call npm i
+call npm install
--- setup/user.bat
+++ setup/user.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d "../frontend/user"
 call npm cache clean --force
-call npm i
+call npm install
