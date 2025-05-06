50a8bf98a0a68d5a2c358485d45ed3f381f4a6e8
ZYD045692 <2214845593@qq.com>
2025-05-06 11:11:37 +0800

    新增JWT说明

--- backend/Djangobackend/utils/config.py
+++ backend/Djangobackend/utils/config.py
@@ -1,9 +1,13 @@
-SMTP_PORT = 0
-SENDER_EMAIL = ""
-SENDER_PASSWORD = ""
+SMTP_PORT = 587
+SENDER_EMAIL = "2214845593@qq.com"
+SENDER_PASSWORD = "lcwldryyxddqdhgf"
 KIMI_API_KEY_LIST = [
+    "sk-gfSJSPrWD6zHx50yDsSzNho37CrIKvf6GWFY9ug5FH9knw08",
+    "sk-cq2HUv1o9kj7gGtz6UFoLaZgxfDHPcixB129K0DjmmlsY9ix", #08
+    "sk-7Qa3HmW1pqmuZZxAvC4vOHneb9myhGOcP3SfrsX9VuaWtsn1", #07
+    "sk-JndvvttUyJK2srwwPhoR4Lg8EKcdhUo9eZbrWpeKUnTnGXUY", #06
 ]
-FFMPEG_DIR = r""
+FFMPEG_DIR = r"D:\ffmpeg_full_build\bin\ffmpeg.exe"
 MD_AIGC_DIR = "tempDir"
 PNG_DIR = "tempPng"
 VIDEO_DIR = "tempVideo"
